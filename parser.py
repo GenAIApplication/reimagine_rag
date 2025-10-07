7871a062660367ea4e48c2ea2783e01608752da7
rootmldeveloper-spec <rootml.developer@gmail.com>
2025-10-07 16:10:24 +0530

    llama parse congig updated

--- parser.py
+++ parser.py
@@ -1,35 +1,18 @@
 from config import env_config
 from llama_cloud_services import LlamaParse
 import io
+from typing import Optional
 
 parser = LlamaParse(
-  # See how to get your API key at https://docs.cloud.llamaindex.ai/api_key
-  api_key=env_config.get("LLAMA_PARSE_API_KEY",None),
-
-  # Adaptive long table. LlamaParse will try to detect long table and adapt the output
-  adaptive_long_table=True,
-
-  # Whether to try to extract outlined tables
-  outlined_table_extraction=True,
-
-  # Whether to use high resolution OCR (Slow)
-  high_res_ocr=True,
-
-  # The page separator
-  page_separator="\n\n---\n\n",
-
-  # The model to use
-  model="openai-gpt-4-1-mini",
-
-  # Whether to output tables as HTML in the markdown output
-  output_tables_as_HTML=True,
-
-  # The parsing mode
-  parse_mode="parse_page_with_agent",
+    api_key=env_config.get("LLAMA_PARSE_API_KEY"),
+    parse_mode="parse_page_with_lvm", 
+    model="openai-gpt-4-1-mini",
+    high_res_ocr=False,             
+    adaptive_long_table=True,
+    outlined_table_extraction=True,
+    output_tables_as_HTML=True,
 )
 
-import io
-from typing import Optional
 
 async def parse_pdf(pdf_bytes: bytes, file_name: Optional[str] = None):
     """
